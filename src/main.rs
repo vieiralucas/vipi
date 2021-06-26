70768b8d5c5ba34b2971fc5316e3e6f1ccacfdf7
Lucas Vieira <vieiralucas4@gmail.com>
2021-06-26 17:03:42 -0300

    Introduce update method

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,63 @@
 extern crate termion;
 
-use termion::event::{Key, Event};
+use std::io::{stdin, stdout, Write};
+use termion::event::{Event, Key};
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
-use std::io::{Write, stdout, stdin};
 
 struct Cursor {
     x: u16,
-    y: u16
+    y: u16,
 }
 struct State {
     lines: Vec<String>,
-    cursor: Cursor
+    cursor: Cursor,
 }
 
 impl State {
     fn render(&self, term: &mut impl Write) {
         let mut y = 1;
-        write!(term, "{}{}", termion::clear::All, termion::cursor::Goto(1, 1)).unwrap();
+        write!(
+            term,
+            "{}{}",
+            termion::clear::All,
+            termion::cursor::Goto(1, 1)
+        )
+        .unwrap();
         for line in &self.lines {
             write!(term, "{}", line).unwrap();
             y += 1;
             write!(term, "{}", termion::cursor::Goto(1, y)).unwrap();
         }
-        write!(term, "{}", termion::cursor::Goto(self.cursor.x, self.cursor.y)).unwrap();
+        write!(
+            term,
+            "{}",
+            termion::cursor::Goto(self.cursor.x, self.cursor.y)
+        )
+        .unwrap();
 
         term.flush().unwrap();
+    }
+
+    fn update(&mut self, evt: Event) -> bool {
+        match evt {
+            Event::Key(Key::Char('q')) => return true,
+            Event::Key(Key::Char('h')) => {
+                self.cursor.x -= 1;
+            }
+            Event::Key(Key::Char('j')) => {
+                self.cursor.y += 1;
+            }
+            Event::Key(Key::Char('k')) => {
+                self.cursor.y -= 1;
+            }
+            Event::Key(Key::Char('l')) => {
+                self.cursor.x += 1;
+            }
+            _ => {}
+        }
+
+        false
     }
 }
 
@@ -34,30 +66,20 @@ fn main() {
     let mut stdout = stdout().into_raw_mode().unwrap();
 
     let mut state = State {
-        lines: vec!["Hello, World".to_string(), "Line below".to_string(), "Line 3".to_string()],
-        cursor: Cursor { x: 1, y: 1 }
+        lines: vec![
+            "Hello, World".to_string(),
+            "Line below".to_string(),
+            "Line 3".to_string(),
+        ],
+        cursor: Cursor { x: 1, y: 1 },
     };
 
-
     state.render(&mut stdout);
-
     for c in stdin.events() {
         let evt = c.unwrap();
-        match evt {
-            Event::Key(Key::Char('q')) => break,
-            Event::Key(Key::Char('h')) => {
-                state.cursor.x -= 1;
-            },
-            Event::Key(Key::Char('j')) => {
-                state.cursor.y += 1;
-            },
-            Event::Key(Key::Char('k')) => {
-                state.cursor.y -= 1;
-            },
-            Event::Key(Key::Char('l')) => {
-                state.cursor.x += 1;
-            },
-            _ => {}
+        let quit = state.update(evt);
+        if quit {
+            break;
         }
 
         state.render(&mut stdout);
